e9155b8d0cbd9d4c1cb4d2b0676b75917be05f8d
dsmith <dsmith@localhost>
2010-06-18 09:17:18 -0400

    Slideshows, but not in the sample app

---
+++ example_app/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    ('', include('cms.urls')),
+    (r'^crowdsourcing/', include('crowdsourcing.urls')),
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^media/(?P<path>.*)$',
+     'django.views.static.serve',
+     {'document_root': settings.MEDIA_ROOT}),
+)
